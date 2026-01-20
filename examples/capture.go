6b38f0d4a2fd76a2c2c7c627d8830f87462e0711
Zach Kelling <z@zeekay.ai>
2026-01-19 18:08:43 -0800

    Rebrand posthog-go as hanzoai/insights-go
    - Update module path from github.com/posthog/posthog-go to github.com/hanzoai/insights-go
    - Update all imports in source files, tests, and examples
    - Update README documentation

--- examples/capture.go
+++ examples/capture.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/posthog/posthog-go"
+	"github.com/hanzoai/insights-go"
 )
 
 func TestCapture(projectAPIKey, endpoint string) {
--- examples/error_tracking.go
+++ examples/error_tracking.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/posthog/posthog-go"
+	"github.com/hanzoai/insights-go"
 	"log/slog"
 	"os"
 	"time"
